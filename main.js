b2c8d7f9c1f5ab6939ed9fc72a65d6f6a8ada11b
Oleksandr <oleksandrrud.ua@gmail.com>
2023-01-31 20:22:04 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+// alert("Hello");
+// const resolt = confirm("Are you here?");
+// console.log(resolt);
+
+// const answer = prompt("Вам вже є 18", "20");
+// console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt("Ваше імя", "");
+// answers[1] = prompt("Ваш вік", "");
+// answers[2] = prompt("Ваше імя", "");
+
+// console.log(answers);
+// document.write(answers);
+
+const user = "Ivan";
+alert(`hello, ${user}`);
