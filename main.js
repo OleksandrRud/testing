f40f6d9c705999adeebcf90c265ba1e406d39c24
Oleksandr <oleksandrrud.ua@gmail.com>
2023-02-02 20:45:19 +0200

    add fitback from user

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
 "use strict";
-// alert("Hello");
-// const resolt = confirm("Are you here?");
-// console.log(resolt);
+// // alert("Hello");
+// // const resolt = confirm("Are you here?");
+// // console.log(resolt);
 
-// const answer = prompt("Вам вже є 18", "20");
-// console.log(answer);
+// // const answer = prompt("Вам вже є 18", "20");
+// // console.log(answer);
 
-// const answers = [];
+// // const answers = [];
 
-// answers[0] = prompt("Ваше імя", "");
-// answers[1] = prompt("Ваш вік", "");
-// answers[2] = prompt("Ваше імя", "");
+// // answers[0] = prompt("Ваше імя", "");
+// // answers[1] = prompt("Ваш вік", "");
+// // answers[2] = prompt("Ваше імя", "");
 
-// console.log(answers);
-// document.write(answers);
+// // console.log(answers);
+// // document.write(answers);
 
-const user = "Ivan";
-alert(`hello, ${user}`);
+// const user = "Ivan";
+// alert(`hello, ${user}`);
+const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const a = prompt("один из последнтх просмотркенных фильмов?", ""),
+  b = prompt("На сколько вы бы его оценили ?", ""),
+  c = prompt("один из последнтх просмотркенных фильмов?", ""),
+  d = prompt("На сколько вы бы его оценили ?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
